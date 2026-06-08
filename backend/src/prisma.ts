b1d44996199822ebbeee2ba3e2be26c4f470ac16
procisss <hoyjoel128@gmail.com>
2026-06-08 09:36:18 +0800

    fix prisma libsql

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -1,13 +1,11 @@
 import { PrismaClient } from '@prisma/client';
-import { createClient } from '@libsql/client';
+
 import { PrismaLibSql } from '@prisma/adapter-libsql';
 
-const libsql = createClient({
+const adapter = new PrismaLibSql({
   url: process.env.TURSO_DATABASE_URL || process.env.DATABASE_URL || '',
   authToken: process.env.TURSO_AUTH_TOKEN!,
 });
-
-const adapter = new PrismaLibSql(libsql);
 
 export const prisma = new PrismaClient({
   adapter,
